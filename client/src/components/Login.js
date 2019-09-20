50c9a624e5481952629fef24ea09dc357cd2cead
LC Carrier <lc@LCs-MacBook-Pro.local>
2019-09-20 13:21:01 -0400

    updated files with mvp

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from 'axios';
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+const Login = (props) => {
+ 
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChange = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const onLogin = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/api/login', credentials)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload)
+        props.history.push('/bubbles')
+      })
+  }
+  
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <h1>Login Form</h1>
+      <form onSubmit={onLogin}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button>Log In</button>
+      </form>
     </>
   );
 };
